3042269feb67ab1786bb71898361d811f8789067
DJEKI Essohanam <48683161+djekicesar@users.noreply.github.com>
2020-04-21 19:00:38 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'pgsql';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = ' ec2-54-247-94-127.eu-west-1.compute.amazonaws.com';
+$CFG->dbhost    = 'ec2-54-247-94-127.eu-west-1.compute.amazonaws.com';
 $CFG->dbname    = 'd5066bhvrbogb5';
 $CFG->dbuser    = 'tdspaagrqjkqoh';
 $CFG->dbpass    = '213984ba3d9f3276d3b51c3930153483f37bd8f84725762edbe3311fff0f884b';
