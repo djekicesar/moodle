7e16177cb368248fea0640d5f1911b8842ffb344
DJEKI Essohanam <48683161+djekicesar@users.noreply.github.com>
2020-04-21 18:58:47 +0100

    change data base credentials

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'pgsql';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'ec2-54-247-118-139.eu-west-1.compute.amazonaws.com';
-$CFG->dbname    = 'd3uv586emgp79t';
-$CFG->dbuser    = 'ntgxkwppmhbqjy';
-$CFG->dbpass    = 'c51a6dbb97b04ccdd3d5da95ec33fcf3b2103500c34e8b94cf437f3282ec2f43';
+$CFG->dbhost    = ' ec2-54-247-94-127.eu-west-1.compute.amazonaws.com';
+$CFG->dbname    = 'd5066bhvrbogb5';
+$CFG->dbuser    = 'tdspaagrqjkqoh';
+$CFG->dbpass    = '213984ba3d9f3276d3b51c3930153483f37bd8f84725762edbe3311fff0f884b';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
